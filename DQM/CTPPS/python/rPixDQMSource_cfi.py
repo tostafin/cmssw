0e67f53e9e723b712d58878a8a0af9eaf21c40d9
vladimir <vladimir.popov@cern.ch>
2017-04-08 12:57:46 +0200

    rename pixel dqm files delete old

--- DQM/CTPPS/python/rPixDQMSource_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-rPixDQMSource = cms.EDAnalyzer("RPixDQMSource",
-#    tagRPixDigi = cms.InputTag("RPixDetDigitizer", ""),
-    tagRPixDigi = cms.InputTag("ctppsPixelDigis", ""),
-    tagRPixCluster = cms.InputTag("clusterProd", ""),  
-    RPStatusWord = cms.untracked.uint32(0x8000),
-    verbosity = cms.untracked.uint32(1)
-)

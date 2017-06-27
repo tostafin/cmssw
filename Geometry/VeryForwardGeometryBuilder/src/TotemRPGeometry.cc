219fbf790a5f8d5750c6df05ed5f3f6a518edb15
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-06-27 14:07:17 +0200

    cleaning

--- Geometry/VeryForwardGeometryBuilder/src/TotemRPGeometry.cc
+++ Geometry/VeryForwardGeometryBuilder/src/TotemRPGeometry.cc
@@ -32,7 +32,6 @@ char TotemRPGeometry::Build(const DetGeomDesc *gD)
        or d->name().name().compare(DDD_CTPPS_DIAMONDS_DETECTOR_NAME)==0
        or d->name().name().compare(DDD_CTPPS_PIXELS_DETECTOR_NAME)==0){
 
-      cout << " added " <<d->name().name()<<" ++++++++++++++++++++ "<< endl;
       AddDetector(d->geographicalID(), d);
     }
     // check if it is RP device (primary vacuum)

d5e0acd075501330ab9cf054d9bbcab5dfb432da
nminafra <nicola@cern.ch>
2017-10-11 15:44:14 +0200

    Added temporary GetParameters class

---
+++ RecoCTPPS/TotemRPLocal/interface/CTPPSDiamondTimingGetParameters.h
@@ -0,0 +1,44 @@
+/****************************************************************************
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Laurent Forthomme (laurent.forthomme@cern.ch)
+ *   Nicola Minafra (nicola.minafra@cern.ch)
+ *
+ ****************************************************************************/
+
+#ifndef RecoCTPPS_TotemRPLocal_CTPPSDiamondTimingGetParameters
+#define RecoCTPPS_TotemRPLocal_CTPPSDiamondTimingGetParameters
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/DetSet.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+
+#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
+
+#include <vector>
+#include <unordered_map>
+
+/**
+ * \brief Class retriving parameters for CTPPS Diamond Detectors. TEMP IMPLEMENTATION!
+ * \date Oct 2017
+**/
+class CTPPSDiamondTimingGetParameters
+{
+  public:
+    CTPPSDiamondTimingGetParameters( const edm::ParameterSet& );
+    ~CTPPSDiamondTimingGetParameters();
+
+    float getT0( const CTPPSDiamondDetId& detId );
+    
+    float getPrecision( const CTPPSDiamondDetId& detId );
+    
+    std::vector<float> getTOTParameters( const CTPPSDiamondDetId& detId );
+    
+  private:
+    std::unordered_map<unsigned int, float> t0_map_;
+
+};
+
+#endif

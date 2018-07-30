7581eabf5848648a7788271eaaf8676a83fe2e45
Nicola Minafra <nicola.minafra@cern.ch>
2018-07-30 12:45:15 +0200

    Including Diamond FEDs to DAQ plots

--- DQM/CTPPS/plugins/TotemDAQTriggerDQMSource.cc
+++ DQM/CTPPS/plugins/TotemDAQTriggerDQMSource.cc
@@ -23,13 +23,13 @@
 #include <string>
 
 //----------------------------------------------------------------------------------------------------
- 
+
 class TotemDAQTriggerDQMSource: public DQMEDAnalyzer
 {
   public:
     TotemDAQTriggerDQMSource(const edm::ParameterSet& ps);
     ~TotemDAQTriggerDQMSource() override;
-  
+
   protected:
     void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
     void analyze(edm::Event const& e, edm::EventSetup const& eSetup) override;
@@ -38,7 +38,8 @@ class TotemDAQTriggerDQMSource: public DQMEDAnalyzer
     unsigned int verbosity;
 
     edm::EDGetTokenT<std::vector<TotemFEDInfo>> tokenFEDInfo;
-    
+    edm::EDGetTokenT<std::vector<TotemFEDInfo>> tokenFEDInfoDiamonds;
+
     MonitorElement *daq_bx_diff;
     MonitorElement *daq_event_bx_diff;
     MonitorElement *daq_event_bx_diff_vs_fed;
@@ -56,6 +57,7 @@ TotemDAQTriggerDQMSource::TotemDAQTriggerDQMSource(const edm::ParameterSet& ps) 
   verbosity(ps.getUntrackedParameter<unsigned int>("verbosity", 0))
 {
   tokenFEDInfo = consumes<vector<TotemFEDInfo>>(ps.getParameter<edm::InputTag>("tagFEDInfo"));
+  tokenFEDInfoDiamonds = consumes<vector<TotemFEDInfo>>(ps.getParameter<edm::InputTag>("tagFEDInfoDiamonds"));
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -69,7 +71,7 @@ TotemDAQTriggerDQMSource::~TotemDAQTriggerDQMSource()
 void TotemDAQTriggerDQMSource::bookHistograms(DQMStore::IBooker &ibooker, edm::Run const &, edm::EventSetup const &)
 {
   ibooker.cd();
-  
+
   ibooker.setCurrentFolder("CTPPS/DAQ/");
 
   daq_bx_diff = ibooker.book1D("bx_diff", ";OptoRx_{i}.BX - OptoRx_{j}.BX", 21, -10.5, +10.5);
@@ -84,33 +86,39 @@ void TotemDAQTriggerDQMSource::analyze(edm::Event const& event, edm::EventSetup 
   // get input
   Handle<vector<TotemFEDInfo>> fedInfo;
   event.getByToken(tokenFEDInfo, fedInfo);
+  Handle<vector<TotemFEDInfo>> fedInfoDiamonds;
+  event.getByToken(tokenFEDInfoDiamonds, fedInfoDiamonds);
 
   // check validity
-  bool daqValid = fedInfo.isValid();
+  bool daqValid = fedInfo.isValid()  & fedInfoDiamonds.isValid();
+
+  vector<TotemFEDInfo> fedInfoTot(*fedInfo);
+  fedInfoTot.insert( fedInfoTot.end(), fedInfoDiamonds->begin(), fedInfoDiamonds->end() );
 
   if (!daqValid)
   {
     if (verbosity)
     {
       LogPrint("TotemDAQTriggerDQMSource") <<
-        "WARNING in TotemDAQTriggerDQMSource::analyze > some of the inputs are not valid.\n"
-        << "    fedInfo.isValid = " << fedInfo.isValid();
+        "WARNING in TotemDAQTriggerDQMSource::analyze > some of the inputs are not valid."
+        << "\n    fedInfo.isValid = " << fedInfo.isValid()
+        << "\n    fedInfoDiamonds.isValid = " << fedInfoDiamonds.isValid();
     }
   }
 
   // DAQ plots
   if (daqValid)
   {
-    for (auto &it1 : *fedInfo)
+    for (auto &it1 : fedInfoTot)
     {
       daq_event_bx_diff->Fill(it1.getBX() - event.bunchCrossing());
       daq_event_bx_diff_vs_fed->Fill(it1.getFEDId(), it1.getBX() - event.bunchCrossing());
-  
-      for (auto &it2 : *fedInfo)
+
+      for (auto &it2 : fedInfoTot)
       {
         if (it2.getFEDId() <= it1.getFEDId())
           continue;
-  
+
         daq_bx_diff->Fill(it2.getBX() - it1.getBX());
       }
     }
--- DQM/CTPPS/python/totemDAQTriggerDQMSource_cfi.py
+++ DQM/CTPPS/python/totemDAQTriggerDQMSource_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 totemDAQTriggerDQMSource = DQMEDAnalyzer('TotemDAQTriggerDQMSource',
   tagFEDInfo = cms.InputTag("totemRPRawToDigi", "TrackingStrip"),
+  tagFEDInfoDiamonds = cms.InputTag("ctppsDiamondRawToDigi", "TimingDiamond"),
+
   tagTriggerCounters = cms.InputTag("totemTriggerRawToDigi"),
 
   verbosity = cms.untracked.uint32(0)

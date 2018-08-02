bbb5b9e550905c197f27b691fef2155da91228e0
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-08-02 12:07:08 +0200

    Splitted track recognition algorithm implementation into proper header and source file structure

--- RecoCTPPS/TotemRPLocal/interface/TotemTimingTrackRecognition.h
+++ RecoCTPPS/TotemRPLocal/interface/TotemTimingTrackRecognition.h
@@ -43,110 +43,30 @@ class TotemTimingTrackRecognition
      * "resolution": float
      * "sigma": float
      * "pixelEfficiencyFunction": TF1
-     *
-     * Parameters specific to CTPPSDiamondLocalTrack:
-     * "startFromX": float
-     * "stopAtX": float
-     * "yPosition": float
-     * "yWidth": float
-     *
      * Parameters specific to TotemTimingLocalTrack:
      * "startFromX": float
      * "stopAtX": float
      * "startFromY": float
      * "stopAtY": float
      */
-    TotemTimingTrackRecognition(const edm::ParameterSet& parameters) {
-      //TODO: reading parameters
-    }
+    TotemTimingTrackRecognition(const edm::ParameterSet& parameters);
 
-    ~TotemTimingTrackRecognition() {};
+    ~TotemTimingTrackRecognition();
 
 
 
     // Class public interface:
 
     // Resets internal state of the class instance.
-    void clear() {
-      hitVectorMap.clear();
-    }
+    void clear();
+
 
     // Adds new hit to the set from which the tracks are reconstructed.
-    void addHit(const TotemTimingRecHit& recHit) {
-      hitVectorMap[getHitKey(recHit)].push_back(recHit);
-    }
+    void addHit(const TotemTimingRecHit& recHit);
+
 
     /// Produces a collection of tracks for the current station, given its hits collection
-    int produceTracks(edm::DetSet<TotemTimingLocalTrack>& tracks) {
-
-      int numberOfTracks = 0;
-      DimensionParameters xParam, yParam;
-
-      // TODO: reading parameters for both dimensions
-
-      for(auto hitBatch: hitVectorMap) {
-
-        auto hits = hitBatch.second;
-        std::vector<PartialTrack> xPartTracks, yPartTracks;
-        auto getX = [](const CTPPSTimingRecHit& hit){ return hit.getX(); };
-        auto getXWidth = [](const CTPPSTimingRecHit& hit){ return hit.getXWidth(); };
-        auto getY = [](const CTPPSTimingRecHit& hit){ return hit.getY(); };
-        auto getYWidth = [](const CTPPSTimingRecHit& hit){ return hit.getYWidth(); };
-        producePartialTracks(hits, xParam, getX, getXWidth, xPartTracks);
-        producePartialTracks(hits, yParam, getY, getYWidth, yPartTracks);
-
-        if(xPartTracks.size() == 0 && yPartTracks.size() == 0)
-          continue;
-
-        if(xPartTracks.size() == 0) {
-          PartialTrack partTrackBuffer;
-          getDefaultPartialTrack(hits, getX, getXWidth, partTrackBuffer);
-          xPartTracks.push_back(partTrackBuffer);
-        }
-
-        if(yPartTracks.size() == 0) {
-          PartialTrack partTrackBuffer;
-          getDefaultPartialTrack(hits, getY, getYWidth, partTrackBuffer);
-          yPartTracks.push_back(partTrackBuffer);
-        }
-
-        PartialTrack zTrack;
-        auto getZ = [](const CTPPSTimingRecHit& hit){ return hit.getZ(); };
-        auto getZWidth = [](const CTPPSTimingRecHit& hit){ return hit.getZWidth(); };
-        getDefaultPartialTrack(hits, getZ, getZWidth, zTrack);
-
-        // TODO: unify threshold among different dimensions
-        int validHitsNumber = (int)(xParam.threshold + 1.0);
-
-        for(auto xTrack: xPartTracks) {
-          for(auto yTrack: yPartTracks) {
-
-            int commonHitsNumber = countTrackIntersectionSize(xTrack, yTrack);
-            if(commonHitsNumber >= validHitsNumber) {
-              math::XYZPoint position(
-                (xTrack.end - xTrack.begin) / 2.0,
-                (yTrack.end - yTrack.begin) / 2.0,
-                (zTrack.end - zTrack.begin) / 2.0
-              );
-              math::XYZPoint positionSigma(
-                (xTrack.end + xTrack.begin) / 2.0,
-                (yTrack.end + yTrack.begin) / 2.0,
-                (zTrack.end + zTrack.begin) / 2.0
-              );
-
-              // TODO: setting validity / time / numHits / numPlanes
-
-              TotemTimingLocalTrack newTrack;
-              newTrack.setPosition(position);
-              newTrack.setPositionSigma(positionSigma);
-              tracks.push_back(newTrack);
-            }
-          }
-        }
-      }
-
-      return numberOfTracks;
-    }
+    int produceTracks(edm::DetSet<TotemTimingLocalTrack>& tracks);
 
 
 
@@ -155,19 +75,17 @@ class TotemTimingTrackRecognition
     typedef std::vector<CTPPSTimingRecHit> HitVector;
     typedef std::unordered_map<int, HitVector> HitVectorMap;
 
+
     // Stores CTPPSTimingRecHit vectors grouped by OOTIndex
     HitVectorMap hitVectorMap;
 
 
     // Functions used to extract hit's key by which the hits are grouped in hitVectorMap
     //TODO: template specialization
-    int getHitKey(const CTPPSTimingRecHit& obj) {
-      return 0;
-    }
+    int getHitKey(const CTPPSTimingRecHit& obj);
 
-    int getHitKey(const CTPPSDiamondRecHit& hit) {
-      return hit.getOOTIndex();
-    }
+
+    int getHitKey(const CTPPSDiamondRecHit& hit);
 
 
     /* Structure representing parameters set for single dimension.
@@ -182,6 +100,7 @@ class TotemTimingTrackRecognition
       float rangeEnd;
       TF1 hitFunction;
     };
+
 
     /* Structure describing single "partial track".
      * Partial track is a single dimensional component of a complete track.
@@ -204,31 +123,11 @@ class TotemTimingTrackRecognition
       }
     };
 
-    /* Counts the size of
-     *
+
+    /* Counts the size of the intersection of two partial tracks' hit componetns vectors.
      */
-    int countTrackIntersectionSize(const PartialTrack &track1, const PartialTrack& track2) {
-      auto it1 = track1.hitComponents.begin();
-      auto it2 = track2.hitComponents.begin();
+    int countTrackIntersectionSize(const PartialTrack &track1, const PartialTrack& track2);
 
-      int result = 0;
-
-      while(it1 != track1.hitComponents.end() && it2 != track2.hitComponents.end()) {
-        if(*it1 < *it2)
-          it1++;
-
-        else if(*it1 > *it2)
-          it2++;
-
-        else {
-          result++;
-          it1++;
-          it2++;
-        }
-      }
-
-      return result;
-    }
 
     /* Produces all partial tracks from given set with regard to single dimension.
      *
@@ -246,83 +145,9 @@ class TotemTimingTrackRecognition
         float (*getHitCenter)(const CTPPSTimingRecHit&),
         float (*getHitRangeWidth)(const CTPPSTimingRecHit&),
         std::vector<PartialTrack>& result
-      ) {
+      );
 
-      int numberOfTracks = 0;
-      const double invResolution = 1./param.resolution;
 
-      std::vector<float> hitProfile((param.rangeEnd - param.rangeBegin) * invResolution, 0.);
-      auto hitFunction = param.hitFunction;
-
-      // Creates hit profile
-      for(auto const& hit : hits) {
-
-        float center = getHitCenter(hit);
-        float rangeWidth = getHitRangeWidth(hit);
-        hitFunction.SetParameters(center, rangeWidth, param.sigma);
-
-        for(unsigned int i = 0; i < hitProfile.size(); ++i) {
-          hitProfile[i] += param.hitFunction.Eval(param.rangeBegin + i*param.resolution);
-        }
-      }
-
-      // Guard to make sure that no track is lost
-      hitProfile.push_back(param.threshold - 1.0);
-
-      bool underThreshold = true;
-      float rangeMaximum = -1.0;
-      bool trackRangeFound = false;
-      int trackRangeBegin = 0;
-      int trackRangeEnd;
-
-      // Searches for tracks in the hit profile
-      for(unsigned int i = 0; i < hitProfile.size(); i++) {
-
-        if(hitProfile[i] > rangeMaximum)
-          rangeMaximum = hitProfile[i];
-
-        // Going above the threshold
-        if(underThreshold && hitProfile[i] > param.threshold) {
-          underThreshold = false;
-          trackRangeBegin = i;
-          rangeMaximum = -1.0;
-        }
-
-        // Going under the threshold
-        else if(!underThreshold && hitProfile[i] <= param.threshold){
-          underThreshold = true;
-          trackRangeEnd = true;
-          trackRangeFound = true;
-        }
-
-        // Finds all tracks within the track range
-        if(trackRangeFound) {
-
-          float trackThreshold = rangeMaximum - param.thresholdFromMaximum;
-          int trackBegin;
-          bool underTrackThreshold = true;
-
-          for(int j = trackRangeBegin; j <= trackRangeEnd; j++) {
-
-            if(underTrackThreshold && hitProfile[j] > trackThreshold) {
-              underTrackThreshold = false;
-              trackBegin = j;
-            }
-
-            else if(!underTrackThreshold && hitProfile[j] <= trackThreshold) {
-              underTrackThreshold = true;
-              PartialTrack pt;
-              pt.begin = param.rangeBegin + param.resolution * trackBegin;
-              pt.end = param.rangeBegin + param.resolution * j;
-              result.push_back(pt);
-              numberOfTracks++;
-            }
-          }
-
-          trackRangeFound = false;
-        }
-      }
-    }
 
     /* Creates a partial track that consists of all hits in the hit set.
      *
@@ -340,36 +165,8 @@ class TotemTimingTrackRecognition
         float (*getHitCenter)(const CTPPSTimingRecHit&),
         float (*getHitRangeWidth)(const CTPPSTimingRecHit&),
         PartialTrack& result
-      ) {
+      );
 
-      float minVal;
-      float maxVal;
-      bool initialized = false;
-
-      for(unsigned int i = 0; i < hits.size(); i++) {
-
-        result.hitComponents.insert(i);
-
-        if(initialized) {
-          float newMinVal = getHitCenter(hits[i]) - getHitRangeWidth(hits[i]) / 2.0;
-          float newMaxVal = getHitCenter(hits[i]) + getHitRangeWidth(hits[i]) / 2.0;
-
-          if(minVal > newMinVal)
-            minVal = newMinVal;
-
-          if(maxVal < newMaxVal)
-            maxVal = newMaxVal;
-        }
-
-        else {
-          minVal = getHitCenter(hits[i]) - getHitRangeWidth(hits[i]) / 2.0;
-          maxVal = getHitCenter(hits[i]) + getHitRangeWidth(hits[i]) / 2.0;
-          initialized = true;
-        }
-      }
-
-      return initialized;
-    }
 
     std::vector<DimensionParameters> paramVector;
 };

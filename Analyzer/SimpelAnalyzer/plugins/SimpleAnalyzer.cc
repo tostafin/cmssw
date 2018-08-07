4a2df66deca2e3e957e1731af265f9af3319ae6b
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-08-07 12:03:09 +0200

    Further TotemTimingLocalTrack analysis fixes

--- Analyzer/SimpelAnalyzer/plugins/SimpleAnalyzer.cc
+++ Analyzer/SimpelAnalyzer/plugins/SimpleAnalyzer.cc
@@ -25,6 +25,7 @@
 #include <map>
 
 #include "TH1.h"
+#include "TH2.h"
 #include "TGraph.h"
 
 class SimpleAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
@@ -40,8 +41,11 @@ class SimpleAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       virtual void endJob() override;
 
-      void initHistograms( const TotemTimingDetId&);
       void initTrackHistogram(const TotemTimingDetId&);
+      void initEventTrackPlot(const int eventNo, const TotemTimingDetId& detId);
+
+      void eventTrackPlotAddHit(int eventNo, const TotemTimingRecHit& hit, const TotemTimingDetId& detId);
+      void eventTrackPlotAddTrack(int eventNo, const TotemTimingLocalTrack& track, const TotemTimingDetId& detId);
 
       // ---------- constants ---------------------------
       static const double SAMPIC_MAX_NUMBER_OF_SAMPLES;
@@ -56,14 +60,14 @@ class SimpleAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 
       // ---------- directories ---------------------------
       std::map< TotemTimingDetId, TFileDirectory > maindir_map_;
+      std::map< int, TFileDirectory > eventDirMap;
 
       // ---------- histograms ---------------------------
-      std::map< TotemTimingDetId, TH1F*> yHisto_map_;
-      std::map< TotemTimingDetId, TH1F*> tHisto_map_;
       std::map< TotemTimingDetId, TH1F*> trackXHistoMap;
       std::map< TotemTimingDetId, TH1F*> trackYHistoMap;
       std::map< TotemTimingDetId, TH1F*> hitXHistoMap;
       std::map< TotemTimingDetId, TH1F*> hitYHistoMap;
+      std::map< int, TH2F*> eventTrackMap;
 
       // ---------- graphs ---------------------------
       std::map< TotemTimingDetId, TGraph*> samples_graph_map_;
@@ -102,40 +106,6 @@ SimpleAnalyzer::~SimpleAnalyzer()
 // member functions
 //
 
-void
-SimpleAnalyzer::initHistograms(const TotemTimingDetId& detId)
-{
-  //now do what ever initialization is needed
-  edm::Service<TFileService> fs;
-  if (maindir_map_.find(detId) == maindir_map_.end())
-  {
-    std::string dirName;
-    detId.rpName(dirName, TotemTimingDetId::nPath);
-    std::string chName;
-    detId.channelName(chName, TotemTimingDetId::nFull);
-
-    // create directory for the detector, if not already done
-    maindir_map_[ detId ] = fs->mkdir( dirName );
-
-    // create all histograms
-    std::string yHisto_name(chName);
-    yHisto_name.insert(0, "yDistribution_");
-    yHisto_map_[ detId ] = maindir_map_[ detId ].make<TH1F>(yHisto_name.c_str(), yHisto_name.c_str(), 120, -60, 60 );
-
-    std::string tHisto_name(chName);
-    tHisto_name.insert(0, "tDistribution_");
-    tHisto_map_[ detId ] = maindir_map_[ detId ].make<TH1F>(tHisto_name.c_str(), tHisto_name.c_str(), 100, -20, 20 );
-
-    std::string samples_graph_name(chName);
-    samples_graph_name.insert(0, "samples_graph_");
-    samples_graph_map_[ detId ] = maindir_map_[ detId ].make<TGraph>();
-    samples_graph_map_[ detId ]->SetName(samples_graph_name.c_str());
-    samples_graph_map_[ detId ]->SetTitle(samples_graph_name.c_str());
-    samples_graphCtr_map_[ detId ] = 0;
-  }
-
-}
-
 void SimpleAnalyzer::initTrackHistogram(const TotemTimingDetId& detId) {
   edm::Service<TFileService> fs;
   if (maindir_map_.find(detId) == maindir_map_.end())
@@ -157,19 +127,120 @@ void SimpleAnalyzer::initTrackHistogram(const TotemTimingDetId& detId) {
 
     std::string hitXHistName;
     hitXHistName.insert(0, "xHitDistribution");
-    hitXHistoMap[ detId ] = maindir_map_[ detId ].make<TH1F>(hitXHistName.c_str(), hitXHistName.c_str(), 18, 0, 9 );
+    hitXHistoMap[ detId ] = maindir_map_[ detId ].make<TH1F>(hitXHistName.c_str(), hitXHistName.c_str(), 100, 0, 9 );
 
     std::string hitYHistName;
     hitYHistName.insert(0, "yHitDistribution");
-    hitYHistoMap[ detId ] = maindir_map_[ detId ].make<TH1F>(hitYHistName.c_str(), hitYHistName.c_str(), 20, 45, 65 );
+    hitYHistoMap[ detId ] = maindir_map_[ detId ].make<TH1F>(hitYHistName.c_str(), hitYHistName.c_str(), 800, 45, 65 );
   }
 }
 
+int getArmRPHash(int eventNo, const TotemTimingDetId& detId) {
+  return eventNo * 100 + detId.arm()*10 + detId.rp();
+}
+
+
+void SimpleAnalyzer::initEventTrackPlot(const int eventNo, const TotemTimingDetId& detId) {
+  edm::Service<TFileService> fs;
+
+  //std::string dirName = "event_" + std::to_string(eventNo) + "_track_plot";
+  std::string dirName = "event_" + std::to_string(eventNo) + "_track_plots";
+
+  // create directory for the detector, if not already done
+  eventDirMap[eventNo] = fs->mkdir( dirName );
+
+  float yOffset = (detId.rp() == 0 ? 45. : -65.);
+  // create all histograms
+  std::string plotName = "arm" + std::to_string(detId.arm()) + "_rp" + std::to_string(detId.rp()) + "_hits";
+  eventTrackMap[getArmRPHash(eventNo, detId)] = eventDirMap[eventNo].make<TH2F>(plotName.c_str(), plotName.c_str(), 1500, 0, 51, 500, yOffset, yOffset + 20);
+
+
+  // drawing vertical plane separators
+  auto plot = eventTrackMap[getArmRPHash(eventNo, detId)];
+
+  float X_OFFSET = 10.0;
+
+  auto xAxis = plot->GetXaxis();
+  float X_RESOLUTION = (xAxis->GetXmax() - xAxis->GetXmin()) / (xAxis->GetNbins());
+  auto yAxis = plot->GetYaxis();
+  float Y_RESOLUTION = (yAxis->GetXmax() - yAxis->GetXmin()) / (yAxis->GetNbins());
+
+  int xBins = 0.2 / X_RESOLUTION;
+  int yBins = 20 / Y_RESOLUTION;
+
+  for(int i = 0; i < xBins; i++)
+    for(int j = 0; j < yBins; j++)
+      for(int k = 1; k < 5; k++)
+        plot->Fill(0.5 + k*X_OFFSET + i*X_RESOLUTION + X_RESOLUTION/2.0, yOffset + j*Y_RESOLUTION + Y_RESOLUTION/2.0);
+}
+
+
+void SimpleAnalyzer::eventTrackPlotAddHit(int eventNo, const TotemTimingRecHit& hit, const TotemTimingDetId& detId) {
+
+  if(eventTrackMap.find(getArmRPHash(eventNo, detId)) == eventTrackMap.end())
+    initEventTrackPlot(eventNo, detId);
+
+  auto plot = eventTrackMap[getArmRPHash(eventNo, detId)];
+
+  float X_OFFSET = 10.0;
+
+  auto xAxis = plot->GetXaxis();
+  float X_RESOLUTION = (xAxis->GetXmax() - xAxis->GetXmin()) / (xAxis->GetNbins());
+  auto yAxis = plot->GetYaxis();
+  float Y_RESOLUTION = (yAxis->GetXmax() - yAxis->GetXmin()) / (yAxis->GetNbins());
+
+  float leftX = (1.0 + hit.getX() - hit.getXWidth()/2.0) + X_OFFSET * ((float) detId.plane());
+  float bottomY = hit.getY() - hit.getYWidth()/2.0;
+
+  int xBins = hit.getXWidth() / X_RESOLUTION;
+  int yBins = hit.getYWidth() / Y_RESOLUTION;
+
+  for(int i = 0; i < xBins; i++)
+    for(int j = 0; j < yBins; j++)
+      for(unsigned int value = 0; value < detId.channel()+1; value++)
+        plot->Fill(leftX + i*X_RESOLUTION + X_RESOLUTION/2.0, bottomY + j*Y_RESOLUTION + Y_RESOLUTION/2.0);
+}
+
+void SimpleAnalyzer::eventTrackPlotAddTrack(int eventNo, const TotemTimingLocalTrack& track, const TotemTimingDetId& detId) {
+
+  if(eventTrackMap.find(getArmRPHash(eventNo, detId)) == eventTrackMap.end())
+    initEventTrackPlot(eventNo, detId);
+
+  auto plot = eventTrackMap[getArmRPHash(eventNo, detId)];
+
+  std::cout << "dodaje tracka " << track.getX0() << "," << track.getY0() << " do pota " << detId.rp() << ", arm " << detId.arm() << std::endl;
+
+  float X_OFFSET = 10.0;
+
+  auto xAxis = plot->GetXaxis();
+  float X_RESOLUTION = (xAxis->GetXmax() - xAxis->GetXmin()) / (xAxis->GetNbins());
+  auto yAxis = plot->GetYaxis();
+  float Y_RESOLUTION = (yAxis->GetXmax() - yAxis->GetXmin()) / (yAxis->GetNbins());
+
+  float leftX = (1.0 + track.getX0() - track.getX0Sigma()) + X_OFFSET * 4.0;
+  float bottomY = track.getY0() - track.getY0Sigma();
+
+  std::cout << "leftX:" << leftX << " bottomY:" << bottomY << std::endl;
+
+  int xBins = track.getX0Sigma() * 2.0 / X_RESOLUTION;
+  int yBins = track.getY0Sigma() * 2.0 / Y_RESOLUTION;
+
+  std::cout << "xBins:" << xBins << " yBins:" << yBins << std::endl;
+
+  for(int i = 0; i < xBins; i++)
+    for(int j = 0; j < yBins; j++)
+      plot->Fill(leftX + i*X_RESOLUTION + X_RESOLUTION/2.0, bottomY + j*Y_RESOLUTION + Y_RESOLUTION/2.0);
+}
+
+
 // ------------ method called for each event  ------------
+int eventCounter = 0;
+
 void
 SimpleAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   using namespace edm;
+  eventCounter++;
 
   edm::Handle< edm::DetSetVector<TotemTimingDigi> > timingDigi;
   edm::Handle< edm::DetSetVector<TotemTimingRecHit> > timingRecHit;
@@ -178,52 +249,45 @@ SimpleAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByToken( tokenRecHit_, timingRecHit );
   iEvent.getByToken( tokenLocalTrack_, timingLocalTrack );
 
-  for (const auto& digis : *timingDigi)
-  {
-    const TotemTimingDetId detId( digis.detId() );
-    if (maindir_map_.find(detId) == maindir_map_.end())
-      initHistograms( detId );
-    for (const auto& digi : digis )
-    {
-      // Do stuff on DIGIs
-      int i=0;
-      for ( auto sampIt = digi.getSamplesBegin(); sampIt != digi.getSamplesEnd(); ++sampIt)
-        samples_graph_map_[ detId ]->SetPoint(samples_graphCtr_map_[ detId ]++, SAMPIC_SAMPLING_PERIOD_NS*(i++), *sampIt  );
-    }
-  }
+  bool multipleTracks = false;
 
-  for (const auto& recHits : *timingRecHit)
-  {
-    const TotemTimingDetId detId( recHits.detId() );
-    if (maindir_map_.find(detId) == maindir_map_.end())
-      initHistograms( detId );
-
-    if (maindir_map_.find(detId.getArmId()) == maindir_map_.end())
-      initTrackHistogram( detId.getArmId() );
-
-    for (const auto& recHit : recHits )
-    {
-      // Do stuff on recHits
-      yHisto_map_[ detId ]->Fill( recHit.getY() );
-      tHisto_map_[ detId ]->Fill( recHit.getT() );
-      hitXHistoMap[ detId.getArmId() ]->Fill( recHit.getX() );
-      hitYHistoMap[ detId.getArmId() ]->Fill( recHit.getY() );
-
-    }
-  }
-
-  for (const auto& trackSet : *timingLocalTrack)
-  {
+  for (const auto& trackSet : *timingLocalTrack) {
     const TotemTimingDetId detId( trackSet.detId() );
     if (maindir_map_.find(detId.getArmId()) == maindir_map_.end())
       initTrackHistogram( detId.getArmId() );
-    for (const auto& track : trackSet )
-    {
+
+    if (trackSet.size() > 1)
+      multipleTracks = true;
+
+    for (const auto& track : trackSet ) {
       trackXHistoMap[detId.getArmId()]->Fill(track.getX0());
       trackYHistoMap[detId.getArmId()]->Fill(track.getY0());
-    }
 
+      if(eventCounter <= 10 || multipleTracks) {
+        TotemTimingDetId tmpId(detId);
+        int potId = (track.getY0() > 0 ? 0 : 1);
+        tmpId.setRP(potId);
+        eventTrackPlotAddTrack(eventCounter, track, tmpId);
+      }
+    }
   }
+
+  for (const auto& recHits : *timingRecHit) {
+    const TotemTimingDetId detId( recHits.detId() );
+    if (maindir_map_.find(detId.getArmId()) == maindir_map_.end())
+      initTrackHistogram( detId.getArmId() );
+
+    for (const auto& recHit : recHits ) {
+      // Do stuff on recHits
+      hitXHistoMap[ detId.getArmId() ]->Fill( recHit.getX() );
+      hitYHistoMap[ detId.getArmId() ]->Fill( recHit.getY() );
+
+      if(eventCounter <= 10 || multipleTracks)
+        eventTrackPlotAddHit(eventCounter, recHit, detId);
+    }
+  }
+
+
 }
 
 

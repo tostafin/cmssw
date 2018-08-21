000310bd7445c07af2947e5a021aa81a53ed1a7d
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-08-21 15:43:39 +0200

    Analysis updates

--- Analyzer/SimpelAnalyzer/test/test.py
+++ Analyzer/SimpelAnalyzer/test/test.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 import string
+import os
 
 process = cms.Process('RECODQM')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2000000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.verbosity = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 # minimum of logs
@@ -35,27 +36,15 @@ process.dqmSaver.tag = "CTPPS"
 #     )
 #     )
 # )
+
+source_dir = '/eos/user/n/nminafra/Sampic/319176/'
+source_files = ['file://' + source_dir + filename for filename in os.listdir(source_dir)]
+
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    *(
-        'file:///eos/user/n/nminafra/Sampic/319176/1C0EB6B8-E280-E811-9605-FA163E9626F3.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/1C5EA541-F280-E811-A270-FA163E94E947.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/200C1511-E380-E811-B511-FA163E7AED1B.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/208F4BFE-E680-E811-AC70-FA163EFF1846.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/248732EF-F480-E811-B56D-FA163E746706.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/3EFBA7E5-E680-E811-8DBC-FA163E342326.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/6A5A05D2-F880-E811-88BA-02163E015C05.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/7032DC19-F280-E811-8654-FA163E1A6759.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/A823C49E-F280-E811-AD8E-02163E012DA1.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/AA3933BF-E680-E811-A903-FA163E11B3E0.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/B65F6AC9-E680-E811-89E0-FA163EB67B9F.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/D41C73B1-F280-E811-8682-FA163E048308.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/E25531B6-F280-E811-8AA5-FA163E490F23.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/F25044C7-E680-E811-BCA5-FA163E8A2D90.root',
-        'file:///eos/user/n/nminafra/Sampic/319176/F6BADB7B-EB80-E811-87D4-FA163E333059.root',
+        *source_files
     # 'root:///afs/cern.ch/user/n/nminafra/Work/public/SampicCMS/319190_testAOD.root',
     # '/store/data/Run2018B/TOTEM40/RECO/PromptReco-v2/000/319/190/00000/00A9F18A-B481-E811-9ECD-02163E014BBF.root ',
-    )
     )
 )
 

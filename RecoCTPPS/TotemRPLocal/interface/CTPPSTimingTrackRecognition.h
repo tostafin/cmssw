4d5c97b5cc7676ef75743f13b623f732c41e5c56
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-08-22 15:24:52 +0200

    Implemented template class for general timing track reconstruction

--- RecoCTPPS/TotemRPLocal/interface/CTPPSTimingTrackRecognition.h
+++ RecoCTPPS/TotemRPLocal/interface/CTPPSTimingTrackRecognition.h
@@ -38,40 +38,27 @@ class CTPPSTimingTrackRecognition
 {
   public:
 
-    /* General parameters in the set:
-     * "threshold": float
-     * "thresholdFromMaximum": float
-     * "resolution": float
-     * "sigma": float
-     * "pixelEfficiencyFunction": TF1
-     * Parameters specific to TotemTimingLocalTrack:
-     * "startFromX": float
-     * "stopAtX": float
-     * "startFromY": float
-     * "stopAtY": float
-     */
     CTPPSTimingTrackRecognition(const edm::ParameterSet& parameters);
 
     ~CTPPSTimingTrackRecognition();
 
 
+    // Class API:
 
-    // Class public interface:
-
-    // Resets internal state of the class instance.
+    // Resets internal state of a class instance.
     void clear();
 
 
     // Adds new hit to the set from which the tracks are reconstructed.
-    void addHit(const HIT_TYPE& recHit);
+    virtual void addHit(const HIT_TYPE& recHit) = 0;
 
 
-    /// Produces a collection of tracks for the current station, given its hits collection
-    int produceTracks(edm::DetSet<TRACK_TYPE>& tracks);
+    // Produces a collection of tracks, given its hits collection
+    virtual int produceTracks(edm::DetSet<TRACK_TYPE>& tracks) = 0;
 
 
 
-  private:
+  protected:
 
     // Algorithm parameters:
     float threshold;
@@ -86,18 +73,8 @@ class CTPPSTimingTrackRecognition
     typedef std::unordered_map<int, HitVector> HitVectorMap;
 
 
-    // Stores CTPPSTimingRecHit vectors grouped by OOTIndex
+    /* Stores RecHit vectors that should be processed separately while reconstructing tracks. */
     HitVectorMap hitVectorMap;
-
-
-    // Functions used to extract hit's key by which the hits are grouped in hitVectorMap
-    //TODO: template specialization
-    int getHitKey(const HIT_TYPE& hit);
-
-    int getHitKey(const CTPPSTimingRecHit& obj);
-
-
-    int getHitKey(const CTPPSDiamondRecHit& hit);
 
 
     /* Structure representing parameters set for single dimension.
@@ -118,10 +95,12 @@ class CTPPSTimingTrackRecognition
      *
      * @hits: vector of hits from which the tracks are created
      * @param: describes all parameters used by 1D track recognition algorithm
-     * @getHitCenter: functor extracting hit's center in the dimension
+     * @getHitCenter: function extracting hit's center in the dimension
      *      that the partial tracks relate to
      * @getHitRangeWidth: analogical to getHitCenter, but extracts hit's width
      *      in specific dimension
+     * @setTrackCenter: function used to set track's position in considered dimension
+     * @setTrackSigma: function used to set track's sigma in considered dimension
      * @result: vector to which produced tracks are appended
      */
     void producePartialTracks(
@@ -136,16 +115,13 @@ class CTPPSTimingTrackRecognition
 
 
 
-    /* Creates a partial track that consists of all hits in the hit set.
+    /* Retrieves the bounds of a single dimension range in which all hits are contained.
      *
-     * @hits: vector of hits from which the track is created
-     * @getHitCenter: functor extracting hit's center in the dimension
-     *      that the track relates to
-     * @getHitRangeWidth: analogical to getHitCenter, but extracts hit's width
-     *      in specific dimension
-     * @result: track buffer to which found values are saved
-     *
-     * @return: true if the track was successfully produced. false otherwise
+     * @hits: hits collection to retrieve the range from
+     * @getHitCenter: function extracting hit's center in considered dimension
+     * @getHitRangeWidth: function extracting hit's width in considered dimension
+     * @rangeBegin: buffer to which the range's lower bound is written
+     * @rangeEnd: buffer to which the range's upper bound is written
      */
     bool getHitSpatialRange(
         const HitVector& hits,
@@ -155,5 +131,157 @@ class CTPPSTimingTrackRecognition
         float& rangeEnd
       );
 };
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+template<class TRACK_TYPE, class HIT_TYPE>
+CTPPSTimingTrackRecognition<TRACK_TYPE, HIT_TYPE>::CTPPSTimingTrackRecognition(const edm::ParameterSet& iConfig) :
+    threshold               ( iConfig.getParameter<double>( "threshold" ) ),
+    thresholdFromMaximum    ( iConfig.getParameter<double>( "thresholdFromMaximum" ) ),
+    resolution              ( iConfig.getParameter<double>( "resolution" ) ),
+    sigma                   ( iConfig.getParameter<double>( "sigma" ) ),
+    tolerance               ( iConfig.getParameter<double>( "tolerance" ) ),
+    pixelEfficiencyFunction ( "hit_TF1_CTPPS", iConfig.getParameter<std::string>( "pixelEfficiencyFunction" ).c_str() ) {
+}
+
+
+template<class TRACK_TYPE, class HIT_TYPE>
+CTPPSTimingTrackRecognition<TRACK_TYPE, HIT_TYPE>::~CTPPSTimingTrackRecognition() {};
+
+
+template<class TRACK_TYPE, class HIT_TYPE>
+void CTPPSTimingTrackRecognition<TRACK_TYPE, HIT_TYPE>::clear() {
+  hitVectorMap.clear();
+}
+
+
+template<class TRACK_TYPE, class HIT_TYPE>
+void CTPPSTimingTrackRecognition<TRACK_TYPE, HIT_TYPE>::producePartialTracks(
+    const HitVector& hits,
+    const DimensionParameters& param,
+    float (*getHitCenter)(const HIT_TYPE&),
+    float (*getHitRangeWidth)(const HIT_TYPE&),
+    void (*setTrackCenter)(TRACK_TYPE&, float),
+    void (*setTrackSigma)(TRACK_TYPE&, float),
+    std::vector<TRACK_TYPE>& result
+  ) {
+
+  int numberOfTracks = 0;
+  const double invResolution = 1./param.resolution;
+
+  std::vector<float> hitProfile((param.rangeEnd - param.rangeBegin) * invResolution, 0.);
+  auto hitFunction = param.hitFunction;
+
+  // Creates hit profile
+  for(auto const& hit : hits) {
+
+    float center = getHitCenter(hit);
+    float rangeWidth = getHitRangeWidth(hit);
+
+    hitFunction.SetParameters(center, rangeWidth, param.sigma);
+
+    for(unsigned int i = 0; i < hitProfile.size(); ++i) {
+      hitProfile[i] += hitFunction.Eval(param.rangeBegin + i*param.resolution);
+    }
+  }
+
+  // Guard to make sure that no track is lost
+  hitProfile.push_back(param.threshold - 1.0);
+
+  bool underThreshold = true;
+  float rangeMaximum = -1.0;
+  bool trackRangeFound = false;
+  int trackRangeBegin = 0;
+  int trackRangeEnd;
+
+  // Searches for tracks in the hit profile
+  for(unsigned int i = 0; i < hitProfile.size(); i++) {
+
+    if(hitProfile[i] > rangeMaximum)
+      rangeMaximum = hitProfile[i];
+
+    // Going above the threshold
+    if(underThreshold && hitProfile[i] > param.threshold) {
+      underThreshold = false;
+      trackRangeBegin = i;
+      rangeMaximum = hitProfile[i];
+    }
+
+    // Going under the threshold
+    else if(!underThreshold && hitProfile[i] <= param.threshold){
+      underThreshold = true;
+      trackRangeEnd = i;
+      trackRangeFound = true;
+    }
+
+    // Finds all tracks within the track range
+    if(trackRangeFound) {
+
+      float trackThreshold = rangeMaximum - param.thresholdFromMaximum;
+      int trackBegin;
+      bool underTrackThreshold = true;
+
+      for(int j = trackRangeBegin; j <= trackRangeEnd; j++) {
+
+        if(underTrackThreshold && hitProfile[j] > trackThreshold) {
+          underTrackThreshold = false;
+          trackBegin = j;
+        }
+
+        else if(!underTrackThreshold && hitProfile[j] <= trackThreshold) {
+          underTrackThreshold = true;
+          TRACK_TYPE track;
+          float leftMargin = param.rangeBegin + param.resolution * trackBegin;
+          float rightMargin = param.rangeBegin + param.resolution * j;
+          setTrackCenter(track, (leftMargin + rightMargin) / 2.0);
+          setTrackSigma(track, (rightMargin - leftMargin) / 2.0);
+          result.push_back(track);
+          numberOfTracks++;
+        }
+      }
+
+      trackRangeFound = false;
+    }
+  }
+}
+
+
+template<class TRACK_TYPE, class HIT_TYPE>
+bool CTPPSTimingTrackRecognition<TRACK_TYPE, HIT_TYPE>::getHitSpatialRange(
+    const HitVector& hits,
+    float (*getHitCenter)(const HIT_TYPE&),
+    float (*getHitRangeWidth)(const HIT_TYPE&),
+    float& rangeBegin,
+    float& rangeEnd
+  ) {
+
+  bool initialized = false;
+
+  for(unsigned int i = 0; i < hits.size(); i++) {
+
+    if(initialized) {
+      float bottomVal = getHitCenter(hits[i]) - getHitRangeWidth(hits[i]) / 2.0;
+      float topVal = getHitCenter(hits[i]) + getHitRangeWidth(hits[i]) / 2.0;
+
+      if(bottomVal < rangeBegin)
+        rangeBegin = bottomVal;
+
+      if(topVal > rangeEnd)
+        rangeEnd = topVal;
+    }
+
+    else {
+      rangeBegin = getHitCenter(hits[i]) - getHitRangeWidth(hits[i]) / 2.0;
+      rangeEnd = getHitCenter(hits[i]) + getHitRangeWidth(hits[i]) / 2.0;
+      initialized = true;
+    }
+  }
+
+  return initialized;
+}
+
 
 #endif

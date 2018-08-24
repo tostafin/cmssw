f86ba9d29c11f5cda08ffd06aac408e5cc7a0710
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-08-24 17:43:19 +0200

    test file checkout

--- Analyzer/SimpelAnalyzer/python/CfiFile_cfi.py
+++ Analyzer/SimpelAnalyzer/python/CfiFile_cfi.py
@@ -3,6 +3,4 @@ import FWCore.ParameterSet.Config as cms
 UFSDAnlzr = cms.EDAnalyzer('UFSDAnlzr',
      tagDigi = cms.InputTag("totemTimingRawToDigi","TotemTiming"),
      tagRecHit = cms.InputTag("totemTimingRecHits"),
-     tagLocalTrack = cms.InputTag("totemTimingLocalTracks"),
-     tagStripTrack = cms.InputTag("totemRPLocalTracks"),
 )

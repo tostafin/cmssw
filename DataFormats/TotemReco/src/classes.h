b20979d2b9077751a5ef7b4646801ee0d96dbcda
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-04-27 17:45:10 +0200

    DF: added first versions of digi and rechits formats

--- DataFormats/TotemReco/src/classes.h
+++ DataFormats/TotemReco/src/classes.h
@@ -1,1 +1,8 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/DetSet.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+
+#include "DataFormats/TotemReco/interface/TotemT2Digi.h"
 #include "DataFormats/TotemReco/interface/TotemT2RecHit.h"
+
+#include <vector>

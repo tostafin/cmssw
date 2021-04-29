2c67da221305078c1dd2eea2c0bd414555707f64
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-04-29 21:06:24 +0200

    Geom: Totem geometry has its own record

---
+++ Geometry/Records/interface/PTotemRcd.h
@@ -0,0 +1,6 @@
+#ifndef PTOTEMRCD_H
+#define PTOTEMRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class PTotemRcd : public edm::eventsetup::EventSetupRecordImplementation<PTotemRcd> {};
+#endif

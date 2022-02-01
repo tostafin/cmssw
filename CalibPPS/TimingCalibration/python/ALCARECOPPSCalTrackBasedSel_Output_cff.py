7d8f47d05a8647da0989bf5c30cc7ee59ef0c2b1
grzanka <grzanka@agh.edu.pl>
2022-02-01 13:19:35 +0100

    cleaning

--- CalibPPS/TimingCalibration/python/ALCARECOPPSCalTrackBasedSel_Output_cff.py
+++ CalibPPS/TimingCalibration/python/ALCARECOPPSCalTrackBasedSel_Output_cff.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-# OutALCARECOPPSCalTrackBasedSel_noDrop = cms.PSet(
-#     SelectEvents = cms.untracked.PSet(
-#         SelectEvents = cms.vstring('pathALCARECOPPSCalTrackBasedSel')
-#     ),
-#     outputCommands = cms.untracked.vstring(
-#         'keep *_ALCARECOPPSCalTrackBasedSel_*_*',
-#         'keep *_ctppsDiamondRawToDigi_*_*',
-#         'keep *_totemTimingRawToDigi_*_*',
-#         'keep *_ctppsLocalTrackLiteProducer_*_*'
-#     )
-# )
+OutALCARECOPPSCalTrackBasedSel_noDrop = cms.PSet(
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOPPSCalTrackBasedSel')
+    ),
+    outputCommands = cms.untracked.vstring(
+        'keep *_ALCARECOPPSCalTrackBasedSel_*_*',
+        'keep *_ctppsDiamondRawToDigi_*_*',
+        'keep *_totemTimingRawToDigi_*_*',
+        'keep *_ctppsLocalTrackLiteProducer_*_*'
+    )
+)
 
-# OutALCARECOPPSCalTrackBasedSel = OutALCARECOPPSCalTrackBasedSel_noDrop.clone()
-# OutALCARECOPPSCalTrackBasedSel.outputCommands.insert(0, 'drop *')
+OutALCARECOPPSCalTrackBasedSel = OutALCARECOPPSCalTrackBasedSel_noDrop.clone()
+OutALCARECOPPSCalTrackBasedSel.outputCommands.insert(0, 'drop *')
---
+++ Calibration/PPSAlCaRecoProducer/test/test.sh
@@ -0,0 +1,10 @@
+cmsDriver.py step2  -s L1Reco,ALCAPRODUCER:PPSCalMaxTracksExpress,ENDJOB \
+--process ALCARECO \
+--scenario pp \
+--era ctpps_2018 \
+--conditions auto:run3_data_express \
+--data  \
+--datatier ALCARECO \
+--eventcontent ALCARECO \
+-n 100  --filein file:/eos/project-c/ctpps/subsystems/Software/Off-line/AlCaTest/outputALCAPPS_single.root \
+--fileout file:step2.root

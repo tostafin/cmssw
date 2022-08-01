17d7efce24a7dbd211d20a96834b9295915ce6c1
Arkadiusz Cwikla <arkadiusz.cwikla98@gmail.com>
2022-08-01 11:30:47 +0200

    make fake digi producer work

--- DQM/CTPPS/plugins/TotemT2DigiProducer.cc
+++ DQM/CTPPS/plugins/TotemT2DigiProducer.cc
@@ -33,6 +33,18 @@ private:
     std::vector<std::string> t2FilesVec_;
     std::unordered_map<unsigned int, std::vector<unsigned int>> detid_vs_chid_;
     std::unique_ptr<TChain> inputTree_;
+    
+    // CONSTANTS
+    // only channel 4 contains meaningful data
+    const short channelNumber_ = 4;
+    // dummy chars just to satisfy TotemT2Digi constructor
+    const unsigned char dummyGeo_ = 'g';
+    const unsigned char dummyId_ = 'i';
+    const unsigned char dummyMarker_ = 'm';
+    // conversion constant between time slice and absolute time (in ns) - but inverted
+    const double timeSliceNsInverted_ = 1024.0 / 25.0;
+    // conversion constant between nanoseconds and seconds
+    const double nsToS_ = std::pow(10, 9);
 
 };
 
@@ -40,12 +52,11 @@ TotemT2DigiProducer::TotemT2DigiProducer(const edm::ParameterSet& iConfig) : t2F
     for (const auto& id_map : iConfig.getParameter<std::vector<edm::ParameterSet>>("idsMapping")){
         unsigned int treeChId = id_map.getParameter<unsigned int>("treeChId");
         detid_vs_chid_[treeChId] = id_map.getParameter<std::vector<unsigned int>>("detId");
-        // edm::LogWarning("Totem T2 digi producer ") << treeChId;
     }
+
     inputTree_ = std::make_unique<TChain>("tree");
 
     for (const auto& fname : t2FilesVec_){
-        // edm::LogWarning("Totem T2 digi producer filename ") << fname;
         inputTree_->Add(fname.c_str());
     }
 
@@ -56,7 +67,6 @@ void TotemT2DigiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
     int eventNum = iEvent.id().event();
     auto digi = std::make_unique<edm::DetSetVector<TotemT2Digi>>();
 
-    // 'eventNo', 'numberOfChannels', 'channelNo', 'MaxAmplitude', 'LeadingEdge', 'TrailingEdge', 'TOT'
     int eventNo;
     int numberOfChannels;
     int channelNo;
@@ -74,23 +84,33 @@ void TotemT2DigiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
     inputTree_->SetBranchAddress("TOT", &tot);
     inputTree_->GetEntry(eventNum);
 
-    edm::LogWarning("Totem T2 digi producer produce") << eventNo << ' ' << numberOfChannels << ' ' << channelNo << ' ' << maxAmplitude << ' ' << leadingEdge << ' ' << trailingEdge << ' ' << tot;
-    // Works up to here
-    if(channelNo == 4){
-        TotemT2Digi digiTmp(); // TODO 
+    if(channelNo == channelNumber_){
+        unsigned short le = (unsigned short) floor(leadingEdge * nsToS_ * timeSliceNsInverted_);
+        unsigned short te = (unsigned short) floor(trailingEdge * nsToS_ * timeSliceNsInverted_);
+        TotemT2Digi digiTmp(dummyGeo_, dummyId_, dummyMarker_, le, te); 
 
-        auto vec = detid_vs_chid_.at(ch_id);
+        auto vec = detid_vs_chid_.at(channelNo);
         for (const auto& id : vec) {
             TotemT2DetId detId(id);
             edm::DetSet<TotemT2Digi>& digis_for_detid = digi->find_or_insert(detId);
             digis_for_detid.push_back(digiTmp);
         }
     }
-    iEvent.put(std::move(digi), "totemT2Digis");
+
+    iEvent.put(std::move(digi), "TotemT2");
 }
 
 void TotemT2DigiProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-    // TODO
+    edm::ParameterSetDescription desc;
+    desc.add<vector<std::string>>("t2FilesVec")->setComment("path to fake T2 root data");
+
+    edm::ParameterSetDescription idmap_valid;
+    idmap_valid.add<unsigned int>("treeChId", 0)->setComment("Channel id retrieved from tree");
+    idmap_valid.add<vector<unsigned int>>("detId")->setComment("mapped TotemT2DetId's for this channel");
+
+    desc.addVPSet("idsMapping", idmap_valid);
+
+    descriptions.add("TotemT2DigiProducer", desc);
 }
 
 DEFINE_FWK_MODULE(TotemT2DigiProducer);
--- DQM/CTPPS/test/totemt2_dqm_test_fake_digi_cfg.py
+++ DQM/CTPPS/test/totemt2_dqm_test_fake_digi_cfg.py
@@ -1,9 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 import string
 
+MAX_NUMBER_OF_EVENTS = 80000
+PATH_TO_FAKE_T2_DATA = "/eos/home-a/acwikla/data/nT2_Nino.root"
+
 process = cms.Process('RECODQM')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(MAX_NUMBER_OF_EVENTS) )
 
 process.source = cms.Source("EmptySource")
 
@@ -22,7 +25,6 @@ process.MessageLogger = cms.Service("MessageLogger",
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 # load DQM framework
 process.load("DQM.Integration.config.environment_cfi")
@@ -31,105 +33,60 @@ process.dqmEnv.eventInfoFolder = "EventInfo"
 process.dqmSaver.path = ""
 process.dqmSaver.tag = "CTPPS"
 
-# process.source = cms.Source('PoolSource',
-#     fileNames = cms.untracked.vstring(
-#         '/store/data/Run2018D/ZeroBias/RAW/v1/000/324/747/00000/97A72F4B-786F-5A48-B97E-C596DD73BD77.root',
-#     ),
-# )
-
-################
-# 2130706432 arm=1 plane=0 channel=0
-# 2130722816 arm=1 plane=0 channel=1
-# 2130739200 arm=1 plane=0 channel=2
-# 2130755584 arm=1 plane=0 channel=3
-# 2130771968 arm=1 plane=1 channel=0
-# 2130788352 arm=1 plane=1 channel=1
-# 2130804736 arm=1 plane=1 channel=2
-# 2130821120 arm=1 plane=1 channel=3
-# 2130837504 arm=1 plane=2 channel=0
-# 2130853888 arm=1 plane=2 channel=1
-# 2130870272 arm=1 plane=2 channel=2
-# 2130886656 arm=1 plane=2 channel=3
-# 2130903040 arm=1 plane=3 channel=0
-# 2130919424 arm=1 plane=3 channel=1
-# 2130935808 arm=1 plane=3 channel=2
-# 2130952192 arm=1 plane=3 channel=3
-# 2130968576 arm=1 plane=4 channel=0
-# 2130984960 arm=1 plane=4 channel=1
-# 2131001344 arm=1 plane=4 channel=2
-# 2131017728 arm=1 plane=4 channel=3
-# 2131034112 arm=1 plane=5 channel=0
-# 2131050496 arm=1 plane=5 channel=1
-# 2131066880 arm=1 plane=5 channel=2
-# 2131083264 arm=1 plane=5 channel=3
-# 2131099648 arm=1 plane=6 channel=0
-# 2131116032 arm=1 plane=6 channel=1
-# 2131132416 arm=1 plane=6 channel=2
-# 2131148800 arm=1 plane=6 channel=3
-# 2131165184 arm=1 plane=7 channel=0
-# 2131181568 arm=1 plane=7 channel=1
-# 2131197952 arm=1 plane=7 channel=2
-# 2131214336 arm=1 plane=7 channel=3
 ids = [
-    2130706432,
-    2130722816,
-    2130739200,
-    2130755584,
-    2130771968,
-    2130788352,
-    2130804736,
-    2130821120,
-    2130837504,
-    2130853888,
-    2130870272,
-    2130886656,
-    2130903040,
-    2130919424,
-    2130935808,
-    2130952192,
-    2130968576,
-    2130984960,
-    2131001344,
-    2131017728,
-    2131034112,
-    2131050496,
-    2131066880,
-    2131083264,
-    2131099648,
-    2131116032,
-    2131132416,
-    2131148800,
-    2131165184,
-    2131181568,
-    2131197952,
-    2131214336
+    # arm 0
+    # plane 0
+    # tile 0    tile 1      tile 2      tile 3
+    2113929216, 2113945600, 2113961984, 2113978368,
+    # plane 1
+    2113994752, 2114011136, 2114027520, 2114043904,
+    # plane 2
+    2114060288, 2114076672, 2114093056, 2114109440,
+    # plane 3
+    2114125824, 2114142208, 2114158592, 2114174976,
+    # plane 4
+    2114191360, 2114207744, 2114224128, 2114240512,
+    # plane 5
+    2114256896, 2114273280, 2114289664, 2114306048,
+    # plane 6
+    2114322432, 2114338816, 2114355200, 2114371584,
+    # plane 7
+    2114387968, 2114404352, 2114420736, 2114437120,
+
+    # arm 1
+    # plane 0
+    # tile 0    tile 1      tile 2      tile 3
+    2130706432, 2130722816, 2130739200, 2130755584,
+    # plane 1
+    2130771968, 2130788352, 2130804736, 2130821120,
+    # plane 2
+    2130837504, 2130853888, 2130870272, 2130886656,
+    # plane 3
+    2130903040, 2130919424, 2130935808, 2130952192,
+    # plane 4
+    2130968576, 2130984960, 2131001344, 2131017728,
+    # plane 5
+    2131034112, 2131050496, 2131066880, 2131083264,
+    # plane 6
+    2131099648, 2131116032, 2131132416, 2131148800,
+    # plane 7
+    2131165184, 2131181568, 2131197952, 2131214336
 ]
-#digi converter
-################
-process.totemT2FakeRawToDigi = cms.EDProducer('TotemT2DigiProducer',
-	#input path of the testbeam data
-	t2FilesVec=cms.vstring("/eos/home-a/acwikla/data/nT2_Nino.root"),
-	################
-	#channel mapping
-	################
+# fake digi producer
+process.totemT2Digis = cms.EDProducer('TotemT2DigiProducer',
+	t2FilesVec=cms.vstring(PATH_TO_FAKE_T2_DATA),
 	idsMapping = cms.VPSet(
 		cms.PSet(detId = cms.vuint32(*ids), treeChId = cms.uint32(4))
 	)
 )
 
-#from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_hlt_relval', '')
-
-# process.load('CalibPPS.ESProducers.totemT2DAQMapping_cff')
-# process.load('EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff')
 process.load('Geometry.ForwardCommonData.totemT22021V2XML_cfi')
 process.load('Geometry.ForwardGeometry.totemGeometryESModule_cfi')
 process.load('RecoPPS.Local.totemT2RecHits_cfi')
 process.load('DQM.CTPPS.totemT2DQMSource_cfi')
 
 process.path = cms.Path(
-    # process.totemT2Digis *
-    process.totemT2FakeRawToDigi *
+    process.totemT2Digis *
     process.totemT2RecHits *
     process.totemT2DQMSource
 )

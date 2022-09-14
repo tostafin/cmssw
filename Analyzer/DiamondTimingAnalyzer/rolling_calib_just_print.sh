eadf964ef2177de0de364ab08242d3015f0423c2
Mateusz Obrzut <mateuszobrzut628@gmail.com>
2022-09-14 18:58:22 +0200

    rolling calib script

---
+++ Analyzer/DiamondTimingAnalyzer/rolling_calib_just_print.sh
@@ -0,0 +1,60 @@
+
+#config
+runNumber=357442
+maxEvents=90000
+experimentName=rollingTest
+resultsFolderName=test_for_loop${runNumber}_maxEv${maxEvents}_${experimentName}
+#local variables
+workerPrefix=${resultsFolderName}/worker_timeshift_${runNumber}_maxEv_${maxEvents}
+sqlFileName=sqlite_file:/afs/cern.ch/user/m/mobrzut/public/timing_sqlcalib/ppsDiamondTiming_calibration${runNumber}.sqlite
+# scritpt
+mkdir $resultsFolderName
+echo "######################################## ##########################################"
+echo "WORKER 0:"
+worker0="cmsRun temp/ExampleConfig_cfg.py " 
+worker0+="outputFileName=${workerPrefix}_loop_0.root "
+# worker0+="calibInput=calib_357440_90k.json "
+worker0+="useSQLFile=True sqlFileName=${sqlFileName} "
+worker0+="maxEventsToProcess=${maxEvents}"
+echo $worker0
+echo "HARVESTER 0:"
+harvester0="cmsRun temp/harvester2.py "
+harvester0+="rootFiles=file:${workerPrefix}_loop_0.root "
+harvester0+="calibOutput=${resultsFolderName}/calib_loop_0.json "
+harvester0+="sqlFileName=${sqlFileName} "
+harvester0+="outputDirectoryRoot=${resultsFolderName}"
+echo $harvester0
+
+
+calibFilesList="${resultsFolderName}/calib_loop_0.json"
+for i in 1 2
+do
+    prev_loop=$(($i-1))
+    if [ $prev_loop -gt 0 ]  
+    then        
+        calibFilesList="${calibFilesList},${resultsFolderName}/calib_loop_${prev_loop}.json"
+    fi
+    echo "**************************************************************************************"
+    echo "WORKER $i"
+    workeri="cmsRun temp/ExampleConfig_cfg.py "
+    workeri+="outputFileName=${workerPrefix}_loop_$i.root "
+    # workeri+="calibInput=calib_loop_$prev_loop.json "
+    workeri+="useSQLFile=True "
+    workeri+="sqlFileName=${sqlFileName} "
+    workeri+="maxEventsToProcess=${maxEvents}" 
+    echo $workeri
+
+    echo "HARVESTET $i"
+    harvesteri="cmsRun temp/harvester2.py "
+    harvesteri+="rootFiles=file:${workerPrefix}_loop_$i.root "
+    # harvesteri+="calibInput=calib_loop_$prev_loop.json "
+    harvesteri+="calibOutput=${resultsFolderName}/calib_loop_$i.json " 
+    harvesteri+="loopIndex=$i "
+    harvesteri+="calibFiles=$calibFilesList " 
+    harvesteri+="sqlFileName=${sqlFileName} "
+    harvesteri+="outputDirectoryRoot=${resultsFolderName} "
+    echo $harvesteri
+done
+echo "--------------------------------------------------------------------------------------------------"
+
+

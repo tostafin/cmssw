9dfa64dfcbc67f93e7df7aceca0b2ac0df3b7644
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-02-24 11:41:52 +0100

    Counting 4 words per channel payload instead of 1

--- EventFilter/CTPPSRawToDigi/interface/TotemT2VFATFrame.h
+++ EventFilter/CTPPSRawToDigi/interface/TotemT2VFATFrame.h
@@ -18,16 +18,27 @@
  * Utilitary namespace to retrieve timing/status information from nT2 VFAT frame
 **/
 namespace totem::nt2::vfat {
+  static constexpr size_t num_words_per_ch = 4;
   /// get timing information for single leading edge
-  inline uint16_t leadingEdgeTime(const VFATFrame& frame, size_t ch_id) { return frame.getData()[2 + ch_id] & 0xffff; }
+  inline uint16_t leadingEdgeTime(const VFATFrame& frame, size_t ch_id) {
+    return frame.getData()[2 + num_words_per_ch * ch_id] & 0xffff;
+  }
   /// get timing information for single trailing edge
-  inline uint16_t trailingEdgeTime(const VFATFrame& frame, size_t ch_id) { return frame.getData()[3 + ch_id] & 0xffff; }
+  inline uint16_t trailingEdgeTime(const VFATFrame& frame, size_t ch_id) {
+    return frame.getData()[3 + num_words_per_ch * ch_id] & 0xffff;
+  }
   /// retrieve this channel marker
-  inline uint8_t channelMarker(const VFATFrame& frame, size_t ch_id) { return frame.getData()[1 + ch_id] & 0x1f; }
+  inline uint8_t channelMarker(const VFATFrame& frame, size_t ch_id) {
+    return frame.getData()[1 + num_words_per_ch * ch_id] & 0x1f;
+  }
   /// retrieve the GEO information for this channel
-  inline uint8_t geoId(const VFATFrame& frame, size_t ch_id) { return frame.getData()[0 + ch_id] & 0xff; }
+  inline uint8_t geoId(const VFATFrame& frame, size_t ch_id) {
+    return frame.getData()[0 + num_words_per_ch * ch_id] & 0xff;
+  }
   /// retrieve this channel identifier
-  inline uint8_t channelId(const VFATFrame& frame, size_t ch_id) { return (frame.getData()[0 + ch_id] >> 8) & 0xff; }
+  inline uint8_t channelId(const VFATFrame& frame, size_t ch_id) {
+    return (frame.getData()[0 + num_words_per_ch * ch_id] >> 8) & 0xff;
+  }
 }  // namespace totem::nt2::vfat
 
 #endif

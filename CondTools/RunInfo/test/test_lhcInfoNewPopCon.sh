02783182a4987517c78a829c4d229dc47b127ffd
Jan Chyczynski <jasiek.ch16@gmail.com>
2023-04-06 01:04:46 +0200

    unit test fix

--- CondTools/RunInfo/test/test_lhcInfoNewPopCon.sh
+++ CondTools/RunInfo/test/test_lhcInfoNewPopCon.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-LOCAL_TEST_DIR=src/CondTools/RunInfo/test
-SCRIPTS_DIR=src/CondTools/RunInfo/python
+SCRIPTS_DIR=${CMSSW_BASE}/src/CondTools/RunInfo/python
 
 function die { echo Failure $1: status $2 ; exit $2 ; }
 
@@ -11,8 +10,7 @@ assert_equal() {
   message="$3"
   
   if [ "$expected" != "$actual" ]; then
-    echo "$message: Expected $expected, but got $actual"
-    exit 1
+    die "$message: Expected $expected, but got $actual" 1
   fi
 }
 
@@ -21,8 +19,7 @@ function assert_found_fills {
   shift
   for fill_nr in "$@"; do
     if ! grep -q "Found fill $fill_nr" "$log_file"; then
-      echo "Error: LHCInfoPerFillPopConAnalyzerDuringFill didn't find fill $fill_nr"
-      exit 1
+      die "LHCInfoPerFillPopConAnalyzerDuringFill didn't find fill $fill_nr" 1
     fi
   done
 }
@@ -33,25 +30,25 @@ echo "testing LHCInfoPerFillPopConAnalyzerEndFill for startTime=\"2022-10-24 01:
 cmsRun ${SCRIPTS_DIR}/LHCInfoPerFillPopConAnalyzerEndFill.py \
     destinationConnection="sqlite_file:lhcinfo_pop_unit_test.db" \
     startTime="2022-10-24 01:00:00.000" endTime="2022-10-24 20:00:00.000" \
-    tag=fill_end_test > fill_end_test.log || die "cmsRun LHCInfoPerFillWriter_cfg.py" $?
+    tag=fill_end_test > fill_end_test.log || die "cmsRun LHCInfoPerFillPopConAnalyzerEndFill.py" $?
 assert_equal 7 `cat fill_end_test.log | grep -E '^Since ' | \
     wc -l` "LHCInfoPerFillPopConAnalyzerEndFill written wrong number of payloads"
-assert_found_fills ls_during_test.log 8307 8309
+assert_found_fills fill_end_test.log 8307 8309
 
 echo "testing LHCInfoPerLSPopConAnalyzerEndFill for startTime=\"2022-10-24 01:00:00.000\" endTime=\"2022-10-24 20:00:00.000\"" 
 cmsRun ${SCRIPTS_DIR}/LHCInfoPerLSPopConAnalyzerEndFill.py \
     destinationConnection="sqlite_file:lhcinfo_pop_unit_test.db" \
     startTime="2022-10-24 01:00:00.000" endTime="2022-10-24 20:00:00.000" \
-    tag=ls_end_test > ls_end_test.log || die "cmsRun LHCInfoPerLSWriter_cfg.py" $?
+    tag=ls_end_test > ls_end_test.log || die "cmsRun LHCInfoPerLSPopConAnalyzerEndFill.py" $?
 assert_equal 169 `cat ls_end_test.log | grep -E '^Since ' | \
     wc -l` "LHCInfoPerLSPopConAnalyzerEndFill written wrong number of payloads"
-assert_found_fills ls_during_test.log 8307 8309
+assert_found_fills ls_end_test.log 8307 8309
 
 echo "testing LHCInfoPerFillPopConAnalyzerDuringFill for startTime=\"2022-10-24 01:00:00.000\" endTime=\"2022-10-24 20:00:00.000\"" 
 cmsRun ${SCRIPTS_DIR}/LHCInfoPerFillPopConAnalyzerDuringFill.py \
     destinationConnection="sqlite_file:lhcinfo_pop_unit_test.db" \
     startTime="2022-10-24 01:00:00.000" endTime="2022-10-24 20:00:00.000" \
-    tag=fill_during_test > fill_during_test.log || die "cmsRun LHCInfoPerFillWriter_cfg.py" $?
+    tag=fill_during_test > fill_during_test.log || die "cmsRun LHCInfoPerFillPopConAnalyzerDuringFill.py" $?
 #TODO Is 5 really correct?
 assert_equal 5 `cat fill_during_test.log | grep -E '^Since ' | \
     wc -l` "LHCInfoPerFillPopConAnalyzerDuringFill written wrong number of payloads"
@@ -61,7 +58,7 @@ echo "testing LHCInfoPerLSPopConAnalyzerDuringFill for startTime=\"2022-10-24 01
 cmsRun ${SCRIPTS_DIR}/LHCInfoPerLSPopConAnalyzerDuringFill.py \
     destinationConnection="sqlite_file:lhcinfo_pop_unit_test.db" \
     startTime="2022-10-24 01:00:00.000" endTime="2022-10-24 20:00:00.000" \
-    tag=ls_during_test > ls_during_test.log || die "cmsRun LHCInfoPerLSWriter_cfg.py" $?
+    tag=ls_during_test > ls_during_test.log || die "cmsRun LHCInfoPerLSPopConAnalyzerDuringFill.py" $?
 assert_equal 1 `cat ls_during_test.log | grep -E '^Since ' | \
     wc -l` "LHCInfoPerLSPopConAnalyzerDuringFill written wrong number of payloads"
 assert_found_fills ls_during_test.log 8307 8309

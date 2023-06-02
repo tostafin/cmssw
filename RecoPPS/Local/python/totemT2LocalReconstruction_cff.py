fa914d4667bb44a4ff754ce99d5e3d405a34795d
Fredrik <fredrik.oljemark@helsinki.fi>
2023-06-02 18:00:43 +0200

    Add T2 geometry XML for use with online DQM calibration

--- RecoPPS/Local/python/totemT2LocalReconstruction_cff.py
+++ RecoPPS/Local/python/totemT2LocalReconstruction_cff.py
@@ -3,6 +3,9 @@ import FWCore.ParameterSet.Config as cms
 # reco hit production
 from RecoPPS.Local.totemT2RecHits_cfi import *
 
+# not in database?
+process.load('Geometry.ForwardCommonData.totemT22021V2XML_cfi')
+process.load('Geometry.ForwardGeometry.totemGeometryESModule_cfi')
 
 totemT2LocalReconstructionTask = cms.Task(
     totemT2RecHits 

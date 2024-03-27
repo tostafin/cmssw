dd701c51fd53c3c03712d00a5a18840da307cb06
Jan Chyczynski <jasiek.ch16@gmail.com>
2024-03-27 14:11:20 +0100

    Add lumiid timetype support for LHCInfoPerFill printing

--- CondTools/RunInfo/test/printLHCInfoPerPayloads.py
+++ CondTools/RunInfo/test/printLHCInfoPerPayloads.py
@@ -76,9 +76,10 @@ if options.timetype not in supported_timetypes:
 for line in map(str.rstrip, sys.stdin):
     for iov in line.split():
         if options.record == "LHCInfoPerLS":
-            os.system(f"cmsRun {TEST_DIR}/LHCInfoPerLSAnalyzer_cfg.py db={options.db} tag={options.tag} timetype={options.timetype} \
-            csv={options.csv} header={options.header} separator={options.separator} iov={iov}")
+            os.system(f"cmsRun {TEST_DIR}/LHCInfoPerLSAnalyzer_cfg.py db={options.db} tag={options.tag} \
+            timetype={options.timetype} csv={options.csv} header={options.header} separator={options.separator} iov={iov}")
         elif options.record == "LHCInfoPerFill":
-            os.system(f"cmsRun {TEST_DIR}/LHCInfoPerFillAnalyzer_cfg.py tag={options.tag} db={options.db} timestamp={iov}")
+            os.system(f"cmsRun {TEST_DIR}/LHCInfoPerFillAnalyzer_cfg.py db={options.db} tag={options.tag} \
+            timetype={options.timetype} iov={iov}")
 
         options.header = False

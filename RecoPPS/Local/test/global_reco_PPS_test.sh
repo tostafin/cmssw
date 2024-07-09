6110b69c730a605e162944456255516ac94401da
Christopher <krzysztof.misan@cern.ch>
2024-07-09 12:17:10 +0200

    unit test for RecoPPS

--- RecoPPS/Local/test/global_reco_PPS_test.sh
+++ RecoPPS/Local/test/global_reco_PPS_test.sh
@@ -2,4 +2,4 @@
 TEST_DIR=$CMSSW_BASE/src/RecoPPS/Local/test
 echo "test dir: $TEST_DIR"
 
-cmsRun ${TEST_DIR}/2023_lhcinfo_test_recoCTPPS_cfg.py
+cmsRun ${TEST_DIR}/global_reco_PPS_test_cfg.py maxEvents=50

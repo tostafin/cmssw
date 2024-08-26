ef4541172dfab1f0170dbd81db6ebd084391cd9f
Gustavo <diemort@gmail.com>
2024-08-26 16:08:39 +0200

    updating 2023 flag for ctpps_directSim

--- SimPPS/DirectSimProducer/test/test2023_cfg.py
+++ SimPPS/DirectSimProducer/test/test2023_cfg.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
-process = cms.Process('PPS', eras.Run3)
+from Configuration.Eras.Modifier_ctpps_directSim_cff import ctpps_directSim
+process = cms.Process('PPS', ctpps_directSim, eras.Run3)
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.EventContent.EventContent_cff')
@@ -16,6 +17,15 @@ process.load('RecoPPS.Configuration.recoCTPPS_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.threshold = cms.untracked.string('')
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('detailedInfo','cerr'),
+    detailedInfo = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO') 
+    ),
+    cerr = cms.untracked.PSet(
+        threshold  = cms.untracked.string('WARNING') 
+    )
+)
 
 # global tag
 from Configuration.AlCa.GlobalTag import GlobalTag
@@ -24,10 +34,8 @@ process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun3_2024_realistic_v20
 # raw data source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #'/store/mc/RunIISummer20UL16MiniAODAPVv2/GGToMuMu_Pt-25_Elastic_13TeV-lpair/MINIAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v1/280000/3870E880-4A47-7440-B122-C76062D2290F.root',
-#        '/store/mc/RunIISummer20UL18MiniAODv2/GGToMuMu_Pt-25_Elastic_13TeV-lpair/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/260000/6EC2EE65-77C0-3C43-A51B-7B94FE441894.root',
-        '/store/mc/Run3Summer22DRPremix/GGToMuMu_PT-25_El-El_13p6TeV_lpair/AODSIM/124X_mcRun3_2022_realistic_v12-v2/80000/4b641be6-f3ca-4a8c-9425-c73c7f97675f.root'
-    ),
+    "/store/mc/Run3Summer22DRPremix/GGToMuMu_PT-25_El-El_13p6TeV_lpair/AODSIM/124X_mcRun3_2022_realistic_v12-v2/80000/4b641be6-f3ca-4a8c-9425-c73c7f97675f.root"
+        ),
 )
 
 process.maxEvents = cms.untracked.PSet(
@@ -41,6 +49,9 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 
 from SimPPS.DirectSimProducer.matching_cff import matchDirectSimOutputsAOD
 matchDirectSimOutputsAOD(process)
+
+print('Verbosity of DirectSimProducer = '+str(process.ppsDirectProtonSimulation.verbosity))
+process.ppsDirectProtonSimulation.verbosity = cms.untracked.uint32(1)
 
 #https://github.com/cms-sw/cmssw/blob/1d7ff6911034faa4b6c394e5752484ebd17be3fe/IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py#L67
 #process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetX45 = 0.0859918
@@ -58,7 +69,8 @@ matchDirectSimOutputsAOD(process)
 #process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetY56 = -0.0169734
 #process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetZ56 = -0.0854137
 
-#print(process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetX45)
+print('beamEnergy from profile = '+str(process.profile_2023_default.ctppsLHCInfo.beamEnergy))
+#print('beamEnergy from LHCInfo = '+str(process.ctppsLHCInfo.beamEnergy))
 
 process.p = cms.Path(
     process.directSimPPS

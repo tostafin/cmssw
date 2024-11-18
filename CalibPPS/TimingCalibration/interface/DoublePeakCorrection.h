1363c452b54a9f7f35586c98b29d46c7fcd7f1bc
Tomasz Ostafin <tomasz.dariusz.ostafin@cern.ch>
2024-11-18 18:39:13 +0100

    Separate fitting of two t projections for lower t differences

--- CalibPPS/TimingCalibration/interface/DoublePeakCorrection.h
+++ CalibPPS/TimingCalibration/interface/DoublePeakCorrection.h
@@ -1,7 +1,8 @@
 #ifndef CalibPPS_TimingCalibration_DoublePeakCorrection_h
 #define CalibPPS_TimingCalibration_DoublePeakCorrection_h
 
-#include "CalibPPS/TimingCalibration/interface/PlaneMap.h"
+// #include "CalibPPS/TimingCalibration/interface/PlaneMap.h"
+#include "../interface/PlaneMap.h"
 
 #include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
 
@@ -16,17 +17,15 @@ public:
   void fillLsAndTimeOffset(const TH2F*, const PlaneKey&);
   bool isCorrectionNeeded(const PlaneKey&) const;
   double getCorrectedLeadingTime(const double, const unsigned int, const PlaneKey&) const;
-  double getEncodedLsAndTimeOffset(const PlaneKey& planeKey) const;
+  double getEncodedLsAndTimeOffset(const PlaneKey&) const;
 
 private:
   const TH2F* getTVsLs(TFile&, const std::string&, const CTPPSDiamondDetId&);
-  std::tuple<unsigned int, double, double> findLsAndTimePeaks(const TH2F* tVsLs, const PlaneKey& planeKey) const;
-  double findTimeOffset(const TH2F*, const double, const double) const;
-  double findGaussianMean(const std::unique_ptr<TH1D>& tProjection, const double estimatedMean) const;
+  std::tuple<unsigned int, double, double> findLsAndTimePeaks(const TH2F*, const PlaneKey&) const;
+  double findTimeOffset(const TH2F*, const unsigned int, const double, const double) const;
+  double findGaussianMean(const std::unique_ptr<TH1D>&, const double) const;
 
   std::unordered_map<PlaneKey, std::pair<unsigned int, double>, PlaneKeyHash> lsAndTimeOffsets_;
-
-  static constexpr double TMaxDiff_ = 1.5;
 };
 
 #endif
--- CalibPPS/TimingCalibration/interface/TimingCalibrationData.h
+++ CalibPPS/TimingCalibration/interface/TimingCalibrationData.h
@@ -1,12 +1,23 @@
 #ifndef CalibPPS_TimingCalibration_TimingCalibrationData_h
 #define CalibPPS_TimingCalibration_TimingCalibrationData_h
 
-// #include "CalibPPS/TimingCalibration/interface/DoublePeakCorrection.h"
 #include "DoublePeakCorrection.h"
-// #include "CalibPPS/TimingCalibration/interface/TimingCalibrationHistograms.h"
-#include "TimingCalibrationHistograms.h"
 
-struct TimingCalibrationData : TimingCalibrationHistograms {
+// #include "CalibPPS/TimingCalibration/interface/PlaneMap.h"
+#include "PlaneMap.h"
+
+
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+struct TimingCalibrationData {
+  using PlaneMonitorMap = std::unordered_map<PlaneKey, dqm::reco::MonitorElement*, PlaneKeyHash>;
+  using ChannelMonitorMap = std::unordered_map<uint32_t, dqm::reco::MonitorElement*>;
+
+  ChannelMonitorMap leadingTime;
+  ChannelMonitorMap toT;
+  ChannelMonitorMap leadingTimeVsToT;
+  PlaneMonitorMap leadingTimeVsLs;
+
   DoublePeakCorrection doublePeakCorrection;
 };
 
